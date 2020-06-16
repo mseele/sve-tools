f5461ceb68df13444db4e29f0393cc037057647c
mseele <mseele@gmail.com>
2020-06-16 18:48:32 +0200

    fix: add trim on replace and support upper/lowercase variants

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -4,11 +4,19 @@ function validateEmail(email) {
 }
 
 function replace(content, person) {
+  const firstName = person.firstName.trim()
+  const lastName = person.lastName.trim()
   return content
-    .replace('${vorname}', person.firstName)
-    .replace('${firstName}', person.firstName)
-    .replace('${nachname}', person.lastName)
-    .replace('${lastName}', person.lastName)
+    .replace('${vorname}', firstName)
+    .replace('${VORNAME}', firstName)
+    .replace('${firstName}', firstName)
+    .replace('${firstname}', firstName)
+    .replace('${FIRSTNAME}', firstName)
+    .replace('${nachname}', lastName)
+    .replace('${NACHNAME}', lastName)
+    .replace('${lastName}', lastName)
+    .replace('${lastname}', lastName)
+    .replace('${LASTNAME}', lastName)
 }
 
 export { validateEmail, replace }
