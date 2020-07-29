ced3d39eb11a3773fa3fbfaf7baaab97823885f4
mseele <mseele@gmail.com>
2020-07-29 18:53:28 +0200

    fix(email): use replaceAll instead of replace

--- src/utils/actions.js
+++ src/utils/actions.js
@@ -7,16 +7,16 @@ function replace(content, person) {
   const firstName = person.firstName.trim()
   const lastName = person.lastName.trim()
   return content
-    .replace('${vorname}', firstName)
-    .replace('${VORNAME}', firstName)
-    .replace('${firstName}', firstName)
-    .replace('${firstname}', firstName)
-    .replace('${FIRSTNAME}', firstName)
-    .replace('${nachname}', lastName)
-    .replace('${NACHNAME}', lastName)
-    .replace('${lastName}', lastName)
-    .replace('${lastname}', lastName)
-    .replace('${LASTNAME}', lastName)
+    .replaceAll('${vorname}', firstName)
+    .replaceAll('${VORNAME}', firstName)
+    .replaceAll('${firstName}', firstName)
+    .replaceAll('${firstname}', firstName)
+    .replaceAll('${FIRSTNAME}', firstName)
+    .replaceAll('${nachname}', lastName)
+    .replaceAll('${NACHNAME}', lastName)
+    .replaceAll('${lastName}', lastName)
+    .replaceAll('${lastname}', lastName)
+    .replaceAll('${LASTNAME}', lastName)
 }
 
 function readFile(inputFile) {
